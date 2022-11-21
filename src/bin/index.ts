cfc09db0b9a84423d1a7999e45abbaa6e5d00efc
RedCrafter07 <gh@r07.dev>
2022-11-21 13:15:57 +0100

    Add some stuff to init

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -1,10 +1,17 @@
 #!/usr/bin/env node
 
-import { Argument, Command, Option } from 'commander';
+import { Command, Option } from 'commander';
 import path from 'path';
 import { version } from '../../package.json';
+import { createSpinner } from 'nanospinner';
+import { copyFile } from 'fs/promises';
+import { existsSync } from 'fs';
 
 const program = new Command('jskill');
+
+export const cwd = process.cwd();
+export const mainDir = path.join(__dirname, '..', '..');
+export const configDir = path.join(mainDir, 'src', 'config');
 
 program.version(version, '-v, --version');
 
@@ -17,11 +24,27 @@ program
 			'Prevent pulling the latest version of the config files',
 		).default(true),
 	)
-	.action((options) => {
-		console.log('init', options);
+	.action(async (options) => {
+		const { pull } = options;
 
-		const mainDir = path.join(__dirname, '..', '..');
-		console.log(mainDir);
+		const s = createSpinner('Pulling config files...').start();
+
+		if (pull) {
+			s.warn({ text: 'Pulling is not yet implemented' });
+		}
+
+		s.update({ text: 'Copying config files...' });
+
+		if (await existsSync(path.join(cwd, '.jskillignore'))) {
+			s.error({ text: 'Config files already exist' });
+			return;
+		}
+
+		await copyFile(configDir + '/.jskillignore', cwd + '/.jskillignore');
+
+		s.success({ text: 'Config files copied' });
+
+		process.exit(0);
 	});
 
 program.action(() => {
