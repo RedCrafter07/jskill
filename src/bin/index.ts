6a7d66fb7b72d42afb9faba3ccc2227980a99eaf
RedCrafter07 <gh@r07.dev>
2022-11-22 17:30:14 +0100

    Add caching

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -4,9 +4,10 @@ import { Command, Option } from 'commander';
 import path from 'path';
 import { version } from '../../package.json';
 import { createSpinner } from 'nanospinner';
-import { copyFile } from 'fs/promises';
-import { existsSync } from 'fs';
+import { copyFile, mkdir, readFile, writeFile } from 'fs/promises';
+import { existsSync, fstat } from 'fs';
 import chalk from 'chalk';
+import ignoreParser, { file } from '../lib/ignoreParser';
 
 const program = new Command('jskill');
 
@@ -60,15 +61,97 @@ program
 		).default(false),
 	)
 	.addOption(new Option('--disable-cache', 'Cache config files').default(false))
-	.action((options) => {
+	.action(async (options) => {
 		const { ignoreConfig, disableCache } = options;
 
-		const cache = !disableCache;
-
-		console.log(options, cache);
+		let config: file[] = [];
 
 		if (ignoreConfig)
-			console.log(chalk.yellowBright('[!] Ignoring config files.'));
+			console.log(chalk.yellowBright('[/] Ignoring config files.'));
+
+		if (disableCache) {
+			console.log(chalk.yellowBright('[/] Disabled cache.'));
+			console.log(
+				chalk.yellowBright(
+					'[!] A disabled cache will ignore the cached files and force-parse all jskill config files.',
+				),
+			);
+		} else {
+			const result = await cacheConfig('.jskillignore');
+
+			if (result === false) {
+				console.log(
+					chalk.redBright('[!] An error occurred while caching. Exiting...'),
+				);
+				process.exit(1);
+			}
+
+			config = result;
+		}
 	});
 
 program.parse(process.argv);
+
+async function cacheConfig(filePath: string): Promise<file[] | false> {
+	if (!(await existsSync(path.join(cwd, '.jskillignore')))) {
+		console.log(chalk.redBright('[!] Config file do not exist.'));
+		return false;
+	}
+
+	const cacheDir = path.join(mainDir, '.cache');
+
+	const config = await readFile(path.join(cwd, '.jskillignore'), 'utf8');
+
+	if (!(await existsSync(cacheDir))) {
+		console.log(chalk.yellow("[/] Cache directory doesn't exist."));
+		console.log(chalk.green('[+] Creating cache directory...'));
+		await mkdir(cacheDir);
+
+		console.log(chalk.green('[+] Cache directory created.'));
+	}
+
+	console.log(chalk.yellow('[/] Checking config file existence...'));
+
+	if (!(await existsSync(path.join(cacheDir, '.jskillignore')))) {
+		console.log(chalk.green('[+] Copying config file...'));
+		await copyFile(filePath, path.join(cacheDir, '.jskillignore'));
+		console.log(chalk.green('[+] Config file copied.'));
+	} else {
+		console.log(chalk.yellow('[/] Config file already exist.'));
+		console.log(chalk.yellow('[/] Comparing config file...'));
+
+		const cachedConfig = await readFile(
+			path.join(cacheDir, '.jskillignore'),
+			'utf8',
+		);
+
+		if (cachedConfig === config) {
+			console.log(chalk.green('[+] Config files are the same.'));
+			console.log(chalk.yellow('[/] Not parsing config file again.'));
+
+			return JSON.parse(
+				await readFile(path.join(cacheDir, '.jskillignore'), 'utf8'),
+			);
+		}
+
+		console.log(chalk.yellow('[/] Config files are different.'));
+		console.log(chalk.green('[+] Overriding old config file...'));
+
+		await copyFile(filePath, path.join(cacheDir, '.jskillignore'));
+
+		console.log(chalk.green('[+] Config file overridden.'));
+	}
+
+	console.log(chalk.yellow('[/] Parsing config file...'));
+
+	const result = await ignoreParser(config);
+
+	await writeFile(
+		path.join(cacheDir, '.jskillignore.json'),
+		JSON.stringify(result),
+	);
+
+	console.log(chalk.green('[+] Config file parsed.'));
+
+	return result;
+}
